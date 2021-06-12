7ca79f89cf454e6ad4d88130ba05cf9e119db0b6
Bistuer <fanzhen_email@163.com>
2021-06-12 18:11:00 +0800

    git commit

--- straw-portal/src/main/java/cn/tedu/straw/portal/mapper/UserMapper.java
+++ straw-portal/src/main/java/cn/tedu/straw/portal/mapper/UserMapper.java
@@ -11,6 +11,7 @@ import java.util.List;
 /**
  * <p>
  * Mapper 接口
+ * 使用MybatisPlus
  * </p>
  *
  * @author tedu.cn
@@ -18,6 +19,7 @@ import java.util.List;
  */
 @Repository
 public interface UserMapper extends BaseMapper<User> {
+
 
     //根据用户输入的用户名查询用户信息的方法
     @Select("select * from user where username=#{username}")
