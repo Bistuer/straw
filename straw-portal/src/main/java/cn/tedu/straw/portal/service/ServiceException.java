1832fa9ed70bf2ca1cf96e149b076de26203462b
Bistuer <fanzhen_email@163.com>
2021-06-23 23:03:51 +0800

    "2021年6月23日23:03:38 第一次修改"

--- straw-portal/src/main/java/cn/tedu/straw/portal/service/ServiceException.java
+++ straw-portal/src/main/java/cn/tedu/straw/portal/service/ServiceException.java
@@ -19,7 +19,7 @@ public class ServiceException extends RuntimeException implements Serializable {
     }
 
     public ServiceException(String message) {
-        super(message);
+        super(message); 
     }
 
     public ServiceException(String message, Throwable cause) {
