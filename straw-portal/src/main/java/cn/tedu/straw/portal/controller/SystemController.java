9ac6ed7626790335873191841c5084f2dd7d1529
Bistuer <fanzhen_email@163.com>
2021-09-02 08:48:47 +0800

    "2021年9月2日08:48:10 第一次修改"

--- straw-portal/src/main/java/cn/tedu/straw/portal/controller/SystemController.java
+++ straw-portal/src/main/java/cn/tedu/straw/portal/controller/SystemController.java
@@ -6,12 +6,20 @@ import cn.tedu.straw.portal.vo.R;
 import cn.tedu.straw.portal.vo.RegisterVo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.UUID;
 
 /**
  * @author fanzhen
@@ -82,6 +90,48 @@ public class SystemController {
             return R.failed(e);
         }
 
+    }
+
+    @Value("${straw.resource.path}")
+    private File resourcePath;
+    @Value("${straw.resource.host}")
+    private String resourceHost;
+
+    /**
+     * 接收表单上传的文件
+     */
+    @PostMapping("/upload/file")
+    public R<String> upload(MultipartFile imageFile) throws IOException {
+        /*
+            我们需要保证任何用户上传的文件的文件名都不能重复
+            我们为了尽量避免文件名的重复,采用以下策略
+            1.将原有文件名修改为使用UUID生成的字符串
+            2.不同的日期创建不同的文件夹
+            3.保留文件的扩展名,还能方便文件识别
+         */
+        //按照当前日期创建文件夹
+        String path = DateTimeFormatter.ofPattern("yyyy/MM/dd")
+                .format(LocalDate.now());
+        //path="2020/12/16"
+        File folder = new File(resourcePath, path);
+        //folder->F:/resource/2020/12/16
+        folder.mkdirs();//创建一串文件夹带s的!!!!
+        log.debug("上传的文件夹为:{}", folder.getAbsolutePath());
+        //按照上传文件的原始文件名,保留扩展名xx.xx.jpg
+        // 获得文件名                             012345678
+        String fileName = imageFile.getOriginalFilename();
+        String ext = fileName.substring(fileName.lastIndexOf("."));
+        //使用UUID生成文件名
+        String name = UUID.randomUUID().toString() + ext;
+        log.debug("生成的文件名:{}", name);
+        //F:/resource/2020/12/16/uuid.jpg
+        File file = new File(folder, name);
+        //向硬盘写入文件
+        imageFile.transferTo(file);
+        //直接返回路径方便调用测试
+        String url = resourceHost + "/" + path + "/" + name;
+        log.debug("访问这个文件的路径为:{}", url);
+        return R.ok(url);
     }
 
 }
