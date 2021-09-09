01da09b5551eb3f6d0316e98d4c29eaee9590ed8
Bistuer <fanzhen_email@163.com>
2021-09-09 09:46:23 +0800

    "2021年9月9日09:46:12main 第一次修改"

--- straw-resource/src/main/java/cn/tedu/straw/resource/StrawResourceApplication.java
+++ straw-resource/src/main/java/cn/tedu/straw/resource/StrawResourceApplication.java
@@ -2,8 +2,10 @@ package cn.tedu.straw.resource;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class StrawResourceApplication {
 
     public static void main(String[] args) {
